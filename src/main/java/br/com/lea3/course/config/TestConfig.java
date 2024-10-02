8de81a002cddc2b5d928c63d0f19870f950ecfd7
edivan-car <edivam.c@gmail.com>
2024-10-02 15:39:22 -0300

    Payment, one-to-one association

--- src/main/java/br/com/lea3/course/config/TestConfig.java
+++ src/main/java/br/com/lea3/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.lea3.course.entities.Category;
 import br.com.lea3.course.entities.Order;
 import br.com.lea3.course.entities.OrderItem;
+import br.com.lea3.course.entities.Payment;
 import br.com.lea3.course.entities.Product;
 import br.com.lea3.course.entities.User;
 import br.com.lea3.course.entities.enums.OrderStatus;
@@ -80,7 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepositoy.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
-	
 }
